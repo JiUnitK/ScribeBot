c91e09f59425a58c52052505422b1cc29b8dcd6a
Ji-hoon Kim <jihoon.kim1992@gmail.com>
2021-05-02 17:39:58 -0500

    Added general stats tracking

--- historian.py
+++ historian.py
@@ -14,6 +14,43 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Create bot
 bot = commands.Bot(command_prefix='!')
 
+personality_mirror = {
+    "chaste": "lustful",
+    "lustful": "chase",
+    "energetic": "lazy",
+    "lazy": "energetic",
+    "forgiving": "vengeful",
+    "vengeful": "forgiving",
+    "generous": "selfish",
+    "selfish": "generous",
+    "honest": "deceitful",
+    "deceitful": "hoenst",
+    "just": "arbitrary",
+    "arbitrary": "just",
+    "merciful": "cruel",
+    "cruel": "merciful",
+    "modest": "proud",
+    "proud": "modest",
+    "prudent": "reckless",
+    "reckless": "prudent",
+    "spiritual": "worldly",
+    "wordly": "spiritual",
+    "temperate": "indulgent",
+    "indulgent": "temperate",
+    "trusting": "suspicious",
+    "suspicious": "trusting"
+}
+
+statistics = [
+    "siz", "dex", "str", "con", "app"
+]
+
+skills = [
+    "battle", "siege", "horsemanship", "sword", "lance", "spear", "dagger", "awareness", "boating", "compose", "courtesy",
+    "dancing", "faerie lore", "falconry", "fashion", "first aid", "flirting", "folklore", "gaming", "heraldry", "hunting", "intrigue", "orate", "play", "read", "recognize",
+    "religion", "romance", "singing", "stewardship", "swimming", "tourney"
+]
+
 # Startup Information
 @bot.event
 async def on_ready():
@@ -92,6 +129,11 @@ async def knight(ctx, name):
         await ctx.send("Sir " + name + " is already in the annals of history")
     else:
         data['knights'][name] = {}
+        data['knights'][name]['personality'] = {}
+        data['knights'][name]['passions'] = {}
+        data['knights'][name]['statistics'] = {}
+        data['knights'][name]['skills'] = {}
+        data['knights'][name]['history'] = []
         utility.save(data)
         await ctx.send("Thus marks the chapter of Sir " + name + " in the annals of history")
 
@@ -113,9 +155,6 @@ async def glorify(ctx, *argv):
     data = utility.load()
 
     if name in data['knights']:
-        if not 'history' in data['knights'][name]:
-            data['knights'][name]['history'] = []
-
         data['knights'][name]['history'].append({'glory': int(glory), "reason":event})
         utility.save(data)
         await ctx.send("May the deeds of Sir " + name + " be celebrated for countless generations")
@@ -192,36 +231,232 @@ async def narrate(ctx, *argv):
 
 
 @bot.command()
-async def skill(ctx, skill_name, difficulty, *argv):
-    bonus_list = []
-    for arg in argv:
-        bonus_list += [arg]
+async def set_skill(ctx, skill, value):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+        
+        if skill in personality_mirror:
+            if not skill in knight['personality']:
+                knight['personality'][skill] = {'check': False, 'value': 10}
+                knight['personality'][personality_mirror[skill]] = {'check': False, 'value': 10}
 
-    difficulty = int(difficulty)
+            knight['personality'][skill]['value'] = int(value)
+            knight['personality'][personality_mirror[skill]]['value'] = 20 - int(value)
+            utility.save(data)
+            await ctx.send("Sir " + name + " has " + str(value) + " " + skill + " and " + str(knight['personality'][personality_mirror[skill]]['value']) + " " + personality_mirror[skill])
+        elif skill in skills:
+            if not skill in knight['skills']:
+                knight['skills'][skill] = {'check': False, 'value': 10}
 
-    bonus = 0
-    for x in bonus_list[0:]:
-        bonus += int(x)
+            knight['skills'][skill]['value'] = int(value)
+            utility.save(data)
+            await ctx.send("Sir " + name + " has " + str(value) + " " + skill)
+        elif skill in statistics:
+            if not skill in knight['statistics']:
+                knight['statistics'][skill] = {'check': False, 'value': 10}
 
-    difficulty += bonus
-
-    crit_range = [difficulty, difficulty]
-    if difficulty > 20:
-        crit_range[1] = 20
-        crit_range[0] = 20 - (difficulty - 20)
-
-    roll = random.randint(1, 20)
-    output_str = ctx.author.display_name + " rolled " + skill_name
-    if roll == 1:
-        output_str += " -> fumble\n   (rolled 1)"
-    elif roll >= crit_range[0] and roll <= crit_range[1]:
-        output_str += " -> CRIT!\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
-    elif roll < difficulty:
-        output_str += " -> success!\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
+            knight['statistics'][skill]['value'] = int(value)
+            utility.save(data)
+            await ctx.send("Sir " + name + " has " + str(value) + " " + skill)
+        else:
+            await ctx.send(skill + " is not a valid trait, skill, or statistic")
     else:
-        output_str += " -> fail\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
-    
-    await ctx.send(output_str)
+        await ctx.send("Thou must first claim a knight")
+
+
+@bot.command()
+async def set_passion(ctx, passion, value):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+
+        if not passion in knight['passions']:
+            knight['passions'][passion] = {'check': False, 'value': 10}
+
+        knight['passions'][passion]['value'] = int(value)
+        utility.save(data)
+        await ctx.send("Sir " + name + " has " + str(value) + " " + passion)
+    else:
+        await ctx.send("Thou must first claim a knight")
+
+
+@bot.command()
+async def remove_passion(ctx, passion):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+
+        if passion in knight['passions']:
+            knight['passions'].pop(passion)
+            utility.save(data)
+            await ctx.send("Removed " + passion + " from Sir " + name)
+        else:
+            ctx.send("Sir " + name + " does not have the passion " + passion)
+    else:
+        await ctx.send("Thou must first claim a knight")
+
+
+@bot.command()
+async def check(ctx, skill):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+
+        if skill in knight['personality']:
+            knight['personality'][skill]['check'] = True
+            utility.save(data)
+            await ctx.send("Checked " + skill + " for Sir " + name)
+        elif skill in knight['passions']:
+            knight['passions'][skill]['check'] = True
+            utility.save(data)
+            await ctx.send("Checked " + skill + " for Sir " + name)
+        elif skill in knight['statistics']:
+            knight['statistics'][skill]['check'] = True
+            utility.save(data)
+            await ctx.send("Checked " + skill + " for Sir " + name)
+        elif skill in knight['skills']:
+            knight['skills'][skill]['check'] = True
+            utility.save(data)
+            await ctx.send("Checked " + skill + " for Sir " + name)
+        else:
+            await ctx.send("Sir " + name + " does not have " + skill)
+    else:
+        await ctx.send("Thou must first claim a knight")
+
+
+@bot.command()
+async def uncheck(ctx, skill):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+
+        if skill in knight['personality']:
+            knight['personality'][skill]['check'] = False
+            utility.save(data)
+            await ctx.send("Unchecked " + skill + " for Sir " + name)
+        elif skill in knight['passions']:
+            knight['passions'][skill]['check'] = False
+            utility.save(data)
+            await ctx.send("Unchecked " + skill + " for Sir " + name)
+        elif skill in knight['statistics']:
+            knight['statistics'][skill]['check'] = False
+            utility.save(data)
+            await ctx.send("Unchecked " + skill + " for Sir " + name)
+        elif skill in knight['skills']:
+            knight['skills'][skill]['check'] = False
+            utility.save(data)
+            await ctx.send("Unchecked " + skill + " for Sir " + name)
+        else:
+            await ctx.send("Sir " + name + " does not have " + skill)
+    else:
+        await ctx.send("Thou must first claim a knight")
+
+
+@bot.command()
+async def describe(ctx, *argv):
+    # Parse the argument list
+    arg_list = []
+    for arg in argv:
+        arg_list += [arg]
+
+    data = utility.load()
+
+    knight = {}
+    name = ""
+    if len(argv) > 0:
+        name = arg_list[0]
+        if name in data['knights']:
+            knight = data['knights'][name]
+        else:
+            await ctx.send("The annals of history do not contain the names of every lowborn peasant to walk the earth")
+            return
+    else:
+        if ctx.author.name in data['claims']:
+            name = data['claims'][ctx.author.name]
+            knight = data['knights'][name]
+        else:
+            await ctx.send("Claim a knight or specify the knight you wish to know")
+            return
+
+    if knight:
+        narration = "                                  Sir " + name + ":\n"
+        narration += "---------------------------------------------------------------------\n"
+        narration += "                                  Personality Traits:" + "\n"
+        for key, value in personality_mirror.items():
+            if list(personality_mirror.keys()).index(key) % 2 == 0: # Skip every odd key in personality_mirror
+                if key in knight['personality']:
+                    if knight['personality'][key]['check']:
+                        narration += '[x] '
+                    else:
+                        narration += '[ ] '
+
+                    narration +=  str(key) + ": " + str(knight['personality'][key]['value']) + ", " + str(value) + ": " + str(knight['personality'][value]['value'])
+
+                    if knight['personality'][value]['check']:
+                        narration += ' [x]' + '\n'
+                    else:
+                        narration += ' [ ]' + '\n'
+
+        narration += "---------------------------------------------------------------------\n"
+        narration += "                                         Passions:" + "\n"
+        for key in knight['passions']:
+            if knight['passions'][key]['check']:
+                narration += '[x] '
+            else:
+                narration += '[ ] '
+            narration += str(key) + ": " + str(knight['passions'][key]['value']) + "\n"
+
+        narration += "---------------------------------------------------------------------\n"
+        narration += "                                         Statistics:" + "\n"
+        for key in knight['statistics']:
+            if knight['statistics'][key]['check']:
+                narration += '[x] '
+            else:
+                narration += '[ ] '
+            narration += str(key) + ": " + str(knight['statistics'][key]['value']) + "\n"
+
+        narration += "---------------------------------------------------------------------\n"
+        narration += "                                            Skills:" + "\n"
+        for key in knight['skills']:
+            if knight['skills'][key]['check']:
+                narration += '[x] '
+            else:
+                narration += '[ ] '
+            narration += str(key) + ": " + str(knight['skills'][key]['value']) + "\n"
+
+        await ctx.send(narration)
+
+
+@bot.command()
+async def gm_skill(ctx, skill_name, difficulty, *argv):
+    await ctx.send(ctx.author.display_name + utility.roll(skill_name, difficulty, *argv))
+
+
+@bot.command()
+async def skill(ctx, skill_name, *argv):
+    data = utility.load()
+    if ctx.author.name in data['claims']:
+        name = data['claims'][ctx.author.name]
+        knight = data['knights'][name]
+        
+        if skill_name in knight['personality']:
+            await ctx.send(ctx.author.display_name + utility.roll(skill_name, knight['personality'][skill_name], *argv))
+        elif skill_name in knight['passions']:
+            await ctx.send(ctx.author.display_name + utility.roll(skill_name, knight['passions'][skill_name], *argv))
+        elif skill_name in knight['statistics']:
+            await ctx.send(ctx.author.display_name + utility.roll(skill_name, knight['statistics'][skill_name], *argv))
+        elif skill_name in knight['skills']:
+            await ctx.send(ctx.author.display_name + utility.roll(skill_name, knight['skills'][skill_name], *argv))
+        else:
+            await ctx.send("Sir " + name + " does not have that skill, trait, or passion")
+    else:
+        await ctx.send("Thou must first claim a knight")
 
 
 # Run the bot
--- utility.py
+++ utility.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 def load():
     # Read in the JSON file as it currently exists
@@ -10,3 +11,34 @@ def load():
 def save( data ):
     with open('data/annals.json', 'w') as json_file:
         json.dump(data, json_file)
+
+def roll( skill_name, difficulty, *argv ):
+    bonus_list = []
+    for arg in argv:
+        bonus_list += [arg]
+
+    difficulty = int(difficulty)
+
+    bonus = 0
+    for x in bonus_list[0:]:
+        bonus += int(x)
+
+    difficulty += bonus
+
+    crit_range = [difficulty, difficulty]
+    if difficulty > 20:
+        crit_range[1] = 20
+        crit_range[0] = 20 - (difficulty - 20)
+
+    roll = random.randint(1, 20)
+    output_str = " rolled " + skill_name
+    if roll == 1:
+        output_str += " -> fumble\n   (rolled 1)"
+    elif roll >= crit_range[0] and roll <= crit_range[1]:
+        output_str += " -> CRIT!\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
+    elif roll < difficulty:
+        output_str += " -> success!\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
+    else:
+        output_str += " -> fail\n   (rolled " + str(roll) + ", DC " + str(difficulty) + ", " + "crit range: " + str(crit_range[0]) + " to " + str(crit_range[1]) + ")"
+    
+    return output_str
